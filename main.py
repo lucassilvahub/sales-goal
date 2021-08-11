ce5dff47d53f1488f69a4e584ea550d7f4d7b6bc
Lucas Rebouças Silva <lucasreboucas100@gmail.com>
2021-08-11 09:44:50 -0300

    added main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Solution step-by-step
+# Open the 6 excel files
+# For each file: check if there is any value greater than R$55,000 (Sales column).
+# If there is any value greater than R$55,000 -> Send an sms (Name, Month and sales amount).
+
+# Technologies used: Python, openpyxl, pandas and twilio
