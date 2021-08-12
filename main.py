55ada61abc47e838f59e3ed4480a3fe6e8cd4f1a
Lucas Rebouças Silva <lucasreboucas100@gmail.com>
2021-08-12 10:31:02 -0300

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import pandas as pd
+
 # Solution step-by-step
 # Open the 6 excel files
 # For each file: check if there is any value greater than R$55,000 (Sales column).
 # If there is any value greater than R$55,000 -> Send an sms (Name, Month and sales amount).
 
 # Technologies used: Python, openpyxl, pandas and twilio
+
+list_months = ["January", "February", "March", "April", "May", "June"]
+
+sales_table = pd.read_excel("./Excel spreadsheets/January.xlsx")
+
+print(sales_table)
